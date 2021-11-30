3df4cb5469961002425f569908ea5b8e4fef891e
imad barrahou <imadbarrahou99@gmail.com>
2021-12-01 00:11:35 +0100

    users

---
+++ src/main/java/forumonligne/controllers/UserController.java
@@ -0,0 +1,20 @@
+package forumonligne.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+	public String welcome() {
+		return "welcome to forumeonline backend deployed";
+	}
+	
+	@CrossOrigin()
+	@GetMapping("/users")
+	public String reponse() {
+		return "voici la liste des users";
+	}
+}

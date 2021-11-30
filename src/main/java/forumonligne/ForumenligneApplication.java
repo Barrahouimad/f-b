48229a5d5378a5376f1f6569bbdd859ca2b988dc
imad barrahou <imadbarrahou99@gmail.com>
2021-11-30 23:35:31 +0100

    first commit

---
+++ src/main/java/forumonligne/ForumenligneApplication.java
@@ -0,0 +1,13 @@
+package forumonligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumenligneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumenligneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forumonligne/ForumenligneApplicationTests.java
@@ -0,0 +1,13 @@
+package forumonligne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumenligneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
